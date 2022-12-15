fe6d3fade979912bfd7bffabf781a672d873e5ec
EuniceKrystal <adebanjoeunice01@gmail.com>
2022-12-15 09:35:51 -0800

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,32 +2,22 @@
 
 /**
  * print_square - prints a square
- * @size: print size
- * Reurn: Always 0
+ * @size: print size of the square
+ * Reurn: no return
  */
-
 void print_square(int size)
 {
-	int i;
-	int k;
+	int i, j;
 
-	if (size > 0)
-	{
 	for (i = 0; i < size; i++)
 	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar (350;
+		}
 
-	for (k = 0; k < size; K++)
-	{
-	_putchar (35);
+		if (i != size - 1)
+			_putchar ('\n');
 	}
 	_putchar ('\n');
-
-	}
-
-	}
-
-	else
-	{
-	_putchar ('\n');
-	}
 }
